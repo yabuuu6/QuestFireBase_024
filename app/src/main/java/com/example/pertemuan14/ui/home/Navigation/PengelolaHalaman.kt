82037d7378895770a10950b1079f72ef4408fb5c
buyaramadhan <ytchochote@gmail.com>
2025-01-13 03:56:06 +0700

    menambahkan rute untuk HomeScreen dan InsertMhsView

---
+++ app/src/main/java/com/example/pertemuan14/ui/home/Navigation/PengelolaHalaman.kt
@@ -0,0 +1,38 @@
+package com.example.pertemuan14.ui.home.Navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan14.ui.home.pages.HomeScreen
+import com.example.pertemuan14.ui.home.pages.InsertMhsView
+
+@Composable
+fun PengelolaHalaman (
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+        composable(DestinasiInsert.route){
+            InsertMhsView(
+                onBack = {navController.popBackStack()},
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
