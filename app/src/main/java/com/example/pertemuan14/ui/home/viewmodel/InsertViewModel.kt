c33d06e96bdd68224a0fe4dabb3fb0cb769ed97c
buyaramadhan <ytchochote@gmail.com>
2025-01-13 03:49:14 +0700

    menambahkan InsertViewModel untuk mengelola state for

---
+++ app/src/main/java/com/example/pertemuan14/ui/home/viewmodel/InsertViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.pertemuan14.ui.home.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan14.Repository.RepositoryMhs
+
+
+class InsertViewModel(
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent,
+        )
+    }
+}
